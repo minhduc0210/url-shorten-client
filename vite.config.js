d04abfb803170dc86c92954e400915e77a9b70b4
Enzo Nguyen <haquocminhduc@gmail.com>
2026-01-07 16:37:03 +0700

    AddL not found page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         admin: resolve(__dirname, 'admin.html'),
+        notFound: resolve(__dirname, '404.html'),
       },
     },
   },
